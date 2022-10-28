2ced4798b64bfb357003171855e99bdbd5fa6343
sonali <sonalibuwade7020@gmail.com>
2022-10-28 18:40:38 +0530

    highlight winning and squares

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ board, handleSquareClick }) => {
+const Board = ({ board, handleSquareClick, winningSquares }) => {
   const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
+
     return (
       <Square
         value={board[position]}
         onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
       />
     );
   };
@@ -18,13 +21,11 @@ const Board = ({ board, handleSquareClick }) => {
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-
       <div className="board-row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-
       <div className="board-row">
         {renderSquare(6)}
         {renderSquare(7)}
--- src/components/StatusMessages.jsx
+++ src/components/StatusMessages.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const StatusMessages = ({ winner, current }) => {
-    
-    const noMovesLeft = current.board.every(el => el! == null)
-    return (
-        <h2>
-            {winner && `Winner is ${winner}`}
-            {!winner && !noMovesLeft && `Next player is ${current.isXNext ? 'X' : 'O'}`}
-            {!winner && noMovesLeft && `x and o tied`}
-        </h2>
-    );
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+
+  return (
+    <h2>
+      {winner && `Winner is ${winner}`}
+      {!winner &&
+        !noMovesLeft &&
+        `Next player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMovesLeft && 'X and O tied'}
+    </h2>
+  );
 };
 
-export default StatusMessages;
+export default StatusMessage;
